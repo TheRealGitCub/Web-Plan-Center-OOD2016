5dd6e3cbe376c5ee6e396016c6e8a6976f023cb1
Kobi Tate <kobi@kobitate.com>
2016-07-21 23:07:54 -0400

    Styling menubar and window, background added

--- index.php
+++ index.php
@@ -3,15 +3,59 @@
 
 <head>
 
-	<title>Dev Template</title>
+	<title>Web Plan Center</title>
 	<?php include 'assets/includes/assets.php'; ?>
 
 </head>
 
 <body>
-
-	<?php include 'assets/includes/nav.php'; ?>
 	
+	<div id="main-container">
+	
+		<div id="title-bar">
+			Web Plan Center
+		</div>
+		
+		<div id="action-bar">
+			<div class="action">
+				<div class="action-icon">
+					<i class="fa fa-file fa-fw"></i>
+				</div>
+				<div class="action-text">
+					New Landing Page
+				</div>
+			</div>
+			<div class="action">
+				<div class="action-icon">
+					<i class="fa fa-photo fa-fw"></i>
+				</div>
+				<div class="action-text">
+					New Gallery Page
+				</div>
+			</div>
+			<div class="action">
+				<div class="action-icon">
+					<i class="fa fa-external-link fa-fw"></i>
+				</div>
+				<div class="action-text">
+					New External Link
+				</div>
+			</div>
+			<div class="action action-right">
+				<div class="action-icon">
+					<i class="fa fa-save fa-fw"></i>
+				</div>
+				<div class="action-text">
+					Export Site Layout
+				</div>
+			</div>
+		</div>
+		
+		<div id="content">
+			 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</div>
+		
+	</div>
 
 </body>
 
