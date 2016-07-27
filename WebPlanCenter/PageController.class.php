1ea6859265a2da8e0cb879b35bb6cf209fdc10ca
Kobi Tate <kobi@kobitate.com>
2016-07-26 23:37:10 -0400

    added page function placeholders

--- WebPlanCenter/PageController.class.php
+++ WebPlanCenter/PageController.class.php
@@ -6,6 +6,19 @@
 		public function __construct($model) {
 			$this -> model = $model;
 		}
+		
+		public function newLandingPage($pageModel) {
+			echo ("UNIMPLEMENTED : newLandingPage() - " . $pageModel->title . "\n");
+		}
+		
+		public function newGalleryPage($pageModel) {
+			echo ("UNIMPLEMENTED : newGalleryPage() - " . $pageModel->title . "\n");
+		}
+		
+		public function newExternalLink($pageModel) {
+			echo ("UNIMPLEMENTED : newExternalLink() - " . $pageModel->title . "\n");
+		}
+		
 	}
 
 ?>
